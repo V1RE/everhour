8701250ee04716de7ba07af25330553aa32efa12
Niels Mentink <niels@ment.ink>
2021-09-30 03:45:18 +0200

    moved get request from status to api

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+mod api;
 mod status;
 
 use std::process;
--- src/status.rs
+++ src/status.rs
@@ -1,24 +1,11 @@
 use humantime::format_duration;
-use reqwest::header::CONTENT_TYPE;
-use serde_json::Value;
 use std::error::Error;
 use std::time::Duration;
 
-fn get_timer() -> Result<Value, Box<dyn Error>> {
-    let client = reqwest::blocking::Client::new();
-
-    let res = client
-        .get("https://private-anon-8eb29346f3-everhour.apiary-mock.com/timers/current")
-        .header(CONTENT_TYPE, "application/json")
-        .header("X-Api-Key", "aaaa-bbbb-cccddd-eeefff-gggghhii")
-        .send()?
-        .json::<serde_json::Value>()?;
-
-    Ok(serde_json::json!(res))
-}
+use crate::api;
 
 pub fn status() -> Result<bool, Box<dyn Error>> {
-    let timer = match get_timer() {
+    let timer = match api::get("timers/current".to_string()) {
         Ok(timer) => timer,
         Err(err) => return Err(err),
     };
