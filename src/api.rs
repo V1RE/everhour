2f29a3a809fac8a018e6560c4f0fd154008a74e8
Niels Mentink <niels@ment.ink>
2021-10-04 01:41:07 +0200

    Added more Api tests including one which should only run on CI

--- src/api.rs
+++ src/api.rs
@@ -67,7 +67,7 @@ impl Api {
 #[cfg(test)]
 pub mod tests {
     use super::*;
-    use crate::timer::tests::active_timer_value;
+    use crate::{config::BASE_URL, timer::tests::active_timer_value};
     use httpmock::prelude::*;
 
     pub fn server_api() -> (MockServer, Api) {
@@ -77,20 +77,89 @@ pub mod tests {
     }
 
     #[test]
+    fn test_url() {
+        assert_eq!(BASE_URL.to_string(), API.url("/").to_string())
+    }
+
+    #[ignore]
+    #[test]
+    fn test_timeout() {
+        // Start a lightweight mock server.
+        let (server, api) = server_api();
+
+        // Create a mock on the server.
+        let mock = server.mock(|when, then| {
+            when.method(GET).path("/3");
+            then.status(200)
+                .delay(std::time::Duration::from_secs(3))
+                .json_body(active_timer_value());
+        });
+
+        let err_mock_timeout = server.mock(|when, then| {
+            when.method(GET).path("/11");
+            then.status(200)
+                .delay(std::time::Duration::from_secs(11))
+                .json_body(active_timer_value());
+        });
+
+        let res = api.get("/3");
+        let res_err = api.get("/11");
+
+        mock.assert();
+        err_mock_timeout.assert();
+        assert!(&res.is_ok());
+        assert!(&res_err.is_err());
+    }
+
+    #[test]
     fn test_get() {
         // Start a lightweight mock server.
         let (server, api) = server_api();
 
         // Create a mock on the server.
-        let mock_get_current = server.mock(|when, then| {
-            when.method(GET).path("/timers/current");
-            then.status(200).body(active_timer_value().to_string());
+        let mock = server.mock(|when, then| {
+            when.method(GET).path("/get");
+            then.status(200).json_body(active_timer_value());
         });
 
-        let res = api.get("/timers/current");
+        let err_mock_json = server.mock(|when, then| {
+            when.method(GET).path("/json");
+            then.status(200).body("Non-JSON data");
+        });
 
-        mock_get_current.assert();
+        let res = api.get("/get");
+        let res_err = api.get("/json");
+
+        mock.assert();
+        err_mock_json.assert();
         assert!(&res.is_ok());
         assert_eq!(&res.unwrap(), &active_timer_value());
+        assert!(&res_err.is_err());
+    }
+
+    #[test]
+    fn test_delete() {
+        // Start a lightweight mock server.
+        let (server, api) = server_api();
+
+        // Create a mock on the server.
+        let mock = server.mock(|when, then| {
+            when.method(DELETE).path("/delete");
+            then.status(200).json_body(active_timer_value());
+        });
+
+        let err_mock_json = server.mock(|when, then| {
+            when.method(DELETE).path("/json");
+            then.status(200).body("Non-JSON data");
+        });
+
+        let res = api.delete("/delete");
+        let res_err = api.delete("/json");
+
+        mock.assert();
+        err_mock_json.assert();
+        assert!(&res.is_ok());
+        assert_eq!(&res.unwrap(), &active_timer_value());
+        assert!(&res_err.is_err());
     }
 }
