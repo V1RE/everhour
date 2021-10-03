32642216261ca30d34498617914b0ec2803280cf
Niels Mentink <niels@ment.ink>
2021-10-03 18:45:53 +0200

    Added api endpoint switching

--- src/config.rs
+++ src/config.rs
@@ -1,2 +1,5 @@
 pub const API_KEY: &str = "aaaa-bbbb-cccddd-eeefff-gggghhii";
+#[cfg(not(test))]
 pub const BASE_URL: &str = "https://private-anon-8eb29346f3-everhour.apiary-mock.com/";
+#[cfg(test)]
+pub const BASE_URL: &str = "https://127.0.0.1:8080/";
