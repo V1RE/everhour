43dfce085acb20bb2c03389b37da01a7ff35b702
Niels Mentink <niels@ment.ink>
2021-10-20 01:39:58 +0200

    Updated calculations

--- src/timer.rs
+++ src/timer.rs
@@ -9,11 +9,16 @@ pub fn is_timer_active(timer: Value) -> bool {
 }
 
 pub fn get_timer_duration(timer: Value) -> String {
-    let mut timer_duration = timer["duration"].as_u64().unwrap_or(0);
-    timer_duration = timer["today"].as_u64().unwrap_or(timer_duration);
+    let timer_duration = timer["duration"].as_u64().unwrap_or(0);
+    let timer_today = timer["today"].as_u64().unwrap_or(0);
+    let timer_total = timer_duration + timer_today;
 
-    let duration = Duration::from_secs(timer_duration);
+    let duration = Duration::from_secs(timer_total);
     format_duration(duration).to_string()
+}
+
+pub fn get_timer_name(timer: Value) -> String {
+    timer["task"]["name"].as_str().unwrap_or("").to_string()
 }
 
 pub fn stop() -> Result<bool> {
@@ -33,6 +38,9 @@ pub mod tests {
           "status": "active",
           "duration": 16,
           "today": 10_396,
+          "task": {
+              "name": "active timer"
+            }
         })
     }
 
@@ -48,8 +56,14 @@ pub mod tests {
 
     #[test]
     fn timer_duration() {
-        assert_eq!(get_timer_duration(active_timer_value()), "2h 53m 16s");
+        assert_eq!(get_timer_duration(active_timer_value()), "2h 53m 32s");
         assert_eq!(get_timer_duration(json!({"duration": 7200})), "2h");
         assert_eq!(get_timer_duration(stopped_timer_value()), "0s");
+    }
+
+    #[test]
+    fn timer_name() {
+        assert_eq!(get_timer_name(active_timer_value()), "active timer");
+        assert_eq!(get_timer_name(stopped_timer_value()), "");
     }
 }
